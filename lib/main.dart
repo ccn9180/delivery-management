453e1f22985f7942c38d82b13cbdcf38a7774b1e
ccn9180 <tancn-pm24@student.tarc.edu.my>
2025-08-15 16:13:50 +0800

    try4

--- lib/main.dart
+++ lib/main.dart
@@ -141,7 +141,7 @@ class DeliveryListPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     const Text(
-                      "Try",
+                      "try1",
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
